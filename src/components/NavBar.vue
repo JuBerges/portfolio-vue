9d056e49d9f7bff68516d4d5f287c1966dfb9038
WadeSpectre <mbergesjulien@gmail.com>
2021-03-24 11:16:36 +0100

    nav almost done

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,23 +1,37 @@
 <template>
-    <header id="nav" class="flex justify-between bg-blue-900 h-16 border-b-4 border-blue-500 fixed w-full ">
-        <!--Logo du site-->
-        <div>
-            <router-link @click="scrollTop" :to="{ name: 'Home' }">
-                <div class="ml-1 md:ml-4 flex min-w-max">
-                    <img class="h-11" src="../assets/logo/logo_white_large.png" alt="logo de julien bergès" />
-                </div>
-            </router-link>
+    <header class=" fixed w-full">
+        <div id="nav" class="flex justify-between bg-blue-900 h-16 border-b-4 border-blue-500">
+            <!--Logo du site-->
+            <div class="mt-2">
+                <router-link @click="scrollTop" :to="{ name: 'Home' }">
+                    <div class="ml-2 md:ml-4 flex min-w-max">
+                        <img class="h-11" src="../assets/logo/logo_white_large.png" alt="logo de julien bergès" />
+                    </div>
+                </router-link>
+            </div>
+            <div class="hidden md:flex mr-2 h-11 mt-2">
+                <!-- Lien vers l'accueil-->
+                <router-link @click="scrollTop" class="router btn-nav pt-cust" :to="{ name: 'Home' }">Accueil</router-link>
+                <!-- Lien vers music -->
+                <router-link @click="scrollTop" class="router btn-nav pt-cust" :to="{ name: 'Portfolio' }">
+                    Portfolio
+                </router-link>
+                <!-- Lien vers about -->
+                <router-link @click="scrollTop" class="router btn-nav pt-cust" :to="{ name: 'Contact' }">Contact</router-link>
+            </div>
+            <div @click="toggled()" class="flex md:hidden router bg-white p-2 items-center border-blue-500 border-l-4 px-6 cursor-pointer"><i class="fas fa-bars"></i></div>
         </div>
-        <div class="flex mr-2">
-            <!-- Lien vers l'accueil-->
-            <router-link @click="scrollTop" class="router btn-nav" :to="{ name: 'Home' }">Accueil</router-link>
-            <!-- Lien vers music -->
-            <router-link @click="scrollTop" class="router btn-nav" :to="{ name: 'Portfolio' }">
-                Portfolio
-            </router-link>
-            <!-- Lien vers about -->
-            <router-link @click="scrollTop" class="router btn-nav" :to="{ name: 'Contact' }">Contact</router-link>
-        </div>
+        <transition name="custom-classes-transition" enter-active-class="animated lightSpeedInRight" leave-active-class="animated rollOut">
+            <div v-if="toggle" class="h-16 bg-blue-900 flex justify-center p-2">
+                <router-link @click="scrollTop" class="router btn-nav pt-cust" :to="{ name: 'Home' }">Accueil</router-link>
+                <!-- Lien vers music -->
+                <router-link @click="scrollTop" class="router btn-nav pt-cust" :to="{ name: 'Portfolio' }">
+                    Portfolio
+                </router-link>
+                <!-- Lien vers about -->
+                <router-link @click="scrollTop" class="router btn-nav pt-cust" :to="{ name: 'Contact' }">Contact</router-link>
+            </div>
+        </transition>
     </header>
 </template>
 <script>
@@ -25,17 +39,29 @@ export default {
     name: "NavBar",
     components: {},
     data() {
-        return {};
+        return {
+            toggle: false,
+        };
     },
     methods: {
         scrollTop() {
             return this.$emit("scroll-top");
+        },
+        toggled() {
+            if (!this.toggle) {
+                this.toggle = true;
+            } else {
+                this.toggle = false;
+            }
         },
     },
 };
 </script>
 <!-- Style for darkmode and router-link-activ -->
 <style scoped>
+.pt-cust {
+    padding-top: 14px;
+}
 .router {
     color: rgba(0, 0, 0);
 }
